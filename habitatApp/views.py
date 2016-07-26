729c44a716865d6b7c30250c1235b07efe5abb38
nate-robo <nate.robinson711@gmail.com>
2016-07-26 14:43:39 -0400

    Base HTML templates implemented

--- habitatApp/views.py
+++ habitatApp/views.py
@@ -1,7 +1,18 @@
+"""Django views - front end logic"""
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Donor, Donation
+
 
 def index(request):
-	return HttpResponse("Hello World. You're at the HabitatApp")
+    """Default HttpResponse"""
+    #return HttpResponse("Hello World!")
+    donations = Donation.objects.filter(date__lte=timezone.now()).order_by('date')
+    return render(request, 'habitatApp/index.html', {'donations': donations})
 
-	
+'''
+def add_donation(request):
+    """Do later"""
+    return render(request, 'habitatApp/index.html')
+'''
