4a92fc35b4d5848d211612cf105a64a7ea3c7494
nate-robo <nate.robinson711@gmail.com>
2016-08-10 16:59:06 -0400

    Added Django debug toolbar

--- habitatApp/urls.py
+++ habitatApp/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns, include
+from django.conf import settings
 from . import views
 
 urlpatterns = [
@@ -6,6 +7,13 @@ urlpatterns = [
     url(r'^donation/new/$', views.donation_new, name='donation_new'),
     url(r'^donation/(?P<pk>\d+)/edit/$', views.donation_edit, name='donation_edit'),
     url(r'^donation/(?P<pk>\d+)/$', views.donation_detail, name='donation_detail'),
+
     url(r'^base$', views.base, name='base'),
     #url(r'^donors$', views.donors, name='donors')
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
